974657f6dd209fec6432f0a0a52652fc0746c14c
Philip <philipharteveld3110@gmail.com>
2022-09-03 20:45:54 +0200

    Overview graph's

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -61,27 +61,7 @@ function Content() {
                   <h1 className="font-bold text-silver">R30 000</h1>
                 </div>
               </div>
-              <div className="px-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-              </div>
+              <div className="px-4"></div>
             </div>
 
             <div className=" py-3 px-3  w-full xl:col-span-3 col-span-2 xl:flex items-center justify-between  xl:space-y-0 space-y-4 xl:space-x-4">
--- src/components/Overview/statistic.js
+++ src/components/Overview/statistic.js
@@ -2,12 +2,95 @@ import React, { useState } from "react";
 import { Data } from "../data";
 import { Progress } from "@chakra-ui/react";
 
+import { Line } from "react-chartjs-2";
+
+import { CategoryScale } from "chart.js";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
 function Statistics() {
+  const Investdata = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Savings",
+        data: [10000, 17000, 15000, 20000, 25000, 20000],
+        fill: true,
+        backgroundColor: "#4D264C",
+        borderColor: "#C02C6E",
+      },
+    ],
+  };
+  const data = {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        label: "Savings",
+        data: [10, 15, 20, 25, 30, 32],
+        fill: true,
+        backgroundColor: "rgba(75,192,192,0.2)",
+        borderColor: "rgba(75,192,192,1)",
+      },
+    ],
+  };
   const [activeTab, setActiveTab] = useState(1);
   return (
-    <div className=" justify-between w-full flex flex-col">
-      <div className=" bg-cardBackground w-1/2 content-start ">
-        <h1 className="text-silver">Savings graph </h1>
+    <div className=" justify-between w-full flex flex-row justify-between">
+      <div className=" bg-cardBackground rounded-lg  ">
+        <div className=" bg-cardBackground  content-start  px-3 py-3 rounded-lg ">
+          <h1 className="text-silver text-center font-bold mb-4">Savings</h1>
+          <div className="mb-4">
+            <Line data={data} />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="">
+          <div className=" bg-cardBackground content-start  px-3 py-3 rounded-lg ">
+            <h1 className="text-silver text-center font-bold mb-4">
+              Invesments
+            </h1>
+            <div className="mb-4">
+              <Line data={Investdata} className="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className=" bg-cardBackground content-start  px-3 py-3 rounded-lg ">
+          <h1 className="text-silver text-center font-bold mb-4">
+            Current Balance
+          </h1>
+          <div className="mb-4">
+            <Line data={Investdata} className="" />
+          </div>
+        </div>
       </div>
     </div>
   );
